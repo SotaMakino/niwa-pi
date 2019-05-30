17741488a833877d7b8e401cb027a697b9c5061b
Sota Makino <sm96kyoto@gmail.com>
2019-05-30 22:53:44 +0900

    file update

--- test-base.py
+++ test-base.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO
 from omxplayer.player import OMXPlayer
 from pathlib import Path
 from time import sleep
+import logging
+logging.basicConfig(level=logging.INFO)
 
 # インターバル
 INTERVAL = 3
@@ -10,23 +12,30 @@ INTERVAL = 3
 SLEEPTIME = 20
 # 使用するGPIO
 GPIO_PIN = 18
+# video url
+VIDEO_PATH = Path("../tests/media/test_media_1.mp4")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.IN)
 
+player.playEvent += lambda _: player_log.info("Play")
+player.pauseEvent += lambda _: player_log.info("Pause")
+player.stopEvent += lambda _: player_log.info("Stop")
+
 if __name__ == '__main__':
     try:
-        print ("処理キャンセル：CTRL+C")
-        cnt = 1
+        print ("start niwa")
         while True:
             # センサー感知
+            print("video start")
             if(GPIO.input(GPIO_PIN) == GPIO.HIGH):
-                VIDEO_PATH = Path("../tests/media/test_media_1.mp4")
                 player = OMXPlayer(VIDEO_PATH)
-                sleep(5)
+                player.play()
+                player.stop()
+
             else:
-                print(GPIO.input(GPIO_PIN)) 
-                time.sleep(INTERVAL)
+                player.pause()
+                # time.sleep(INTERVAL)
     except KeyboardInterrupt:
         print("終了処理中...")
     finally:
