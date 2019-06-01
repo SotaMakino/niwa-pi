17ba5793f6786b79552f35fd1857d99e3d568e67
Sota Makino <sm96kyoto@gmail.com>
2019-06-01 17:56:24 +0900

    update

---
+++ niwa.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+from omxplayer.player import OMXPlayer
+from pathlib import Path
+from time import sleep
+
+
+# using GPIO
+GPIO_PIN = 18
+# video url
+VIDEO_PATH = Path("Path")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.IN)
+
+
+if __name__ == '__main__':
+    try:
+        print ("start niwa")
+        while True:
+            if(GPIO.input(GPIO_PIN) == GPIO.HIGH):
+                player = OMXPlayer(VIDEO_PATH)
+                sleep(5)
+            else:
+                print("no signal")
+                sleep(5)
+    except KeyboardInterrupt:
+        print("interrupted")
+    finally:
+        GPIO.cleanup()
+        print("GPIO clean")
